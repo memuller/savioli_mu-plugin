8a7b459e995d40234af1b5fea220788d7a8567fc
Matheus E. Muller <hello@memuller.com>
2014-08-25 23:50:55 -0300

    Displays config pages only at the main blog.

--- presenters/Base.php
+++ presenters/Base.php
@@ -33,6 +33,38 @@
 					return $actions ;
 				});
 			});
+
+			if(
+				(defined('SAVIOLI_MAIN_SITE') && get_current_blog_id() == SAVIOLI_MAIN_SITE )
+				|| is_main_site()
+			){
+				static::setup_config_page();
+			}
+
+			add_filter('galleria_theme', function($theme){
+				return SHIBA_GALLERY_URL.'js/galleria/themes/folio/galleria.folio.min.js';
+			});
+		}
+
+		static function setup_config_page(){
+			$presenter = get_called_class();
+			add_action('admin_init', function(){
+				register_setting('clinica-savioli_options', 'clinica-savioli_options');
+			});
+			add_action('admin_menu', function() use($presenter){
+				add_submenu_page('options-general.php', 'Conteúdo Externo', 'Conteúdo Externo', 'manage_options', 'clinica-savioli_options', function() use($presenter){
+						$presenter::render('admin/options', array(
+							'active_tab' => isset($_GET['tab']) ? $_GET['tab'] : 'shipping', 
+							'page' => '?page=clinica-savioli_options',
+							'options' => array_merge(array(
+								'blogs' => array(),
+								'blogs_num_posts' => 2,
+								'magento_url' => '',
+								'magento_num_posts' => 3
+							),get_option('clinica-savioli_options', array()))
+						));
+				});	
+			});
 		}
 	}
 ?>
