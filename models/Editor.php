e1f9ff63add85d0c4e985f97c64d9e2f57d968ba
Matheus E. Muller <hello@memuller.com>
2014-08-26 00:58:52 -0300

    Uses local avatar, if any.

--- models/Editor.php
+++ models/Editor.php
@@ -19,7 +19,15 @@
 
 		static function build(){
 			parent::build();
-
+			add_filter('get_avatar', function($avatar, $id_or_email, $size, $default, $alt){
+				$user = new \Savioli\Editor($id_or_email);
+				if($user->avatar){
+					$avatar = sprintf('<img class="avatar photo avatar-%s" 
+						width="%s" height="%s" src="%s"></img>',$size, $size, $size, $user->avatar);
+					
+				}
+				return $avatar;
+			}, 10, 5);
 		}
 
 
