102611df365688c8daa1174fba87729540a0fd09
Matheus E. Muller <hello@memuller.com>
2014-09-04 22:51:32 -0300

    Adds helper functions to determine if we're on the main site.

--- base/Base.php
+++ base/Base.php
@@ -5,7 +5,7 @@
 	class Plugin extends BasePlugin {
 
 		static $db_version = '0.1' ;
-		static $custom_posts = array('Banner', 'Clipping');
+		static $custom_posts = array('Banner', 'Clipping', 'Help');
 		static $custom_taxonomies = array();
 		static $custom_post_formats = array();
 		static $custom_classes = array('MagentoProduct', 'MagentoWidget','BannerWidget', 'ForeignPost');
@@ -30,6 +30,15 @@
 			parent::build();
 			add_filter( 'got_rewrite', '__return_true', 999 );
 		
+		}
+
+		static function main_site(){
+			global $current_site;
+			return defined('SAVIOLI_MAIN_SITE')?  SAVIOLI_MAIN_SITE : $current_site->blog_id; 
+		}
+
+		static function is_main_site(){
+			return static::main_site() == get_current_blog_id();
 		}
 
 	}
