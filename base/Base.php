e5fa8783d95afb911dd1c11003460b69a5a06aef
Matheus E. Muller <hello@memuller.com>
2014-10-14 14:52:46 -0300

    Adds AYVP settings migration.

--- base/Base.php
+++ base/Base.php
@@ -4,8 +4,8 @@
 
 	class Plugin extends BasePlugin {
 
-		static $db_version = '0.1' ;
-		static $custom_posts = array('Banner', 'Clipping', 'Help');
+		static $db_version = '0.2' ;
+		static $custom_posts = array('Banner', 'Clipping', 'Video', 'Help');
 		static $custom_taxonomies = array();
 		static $custom_post_formats = array();
 		static $custom_classes = array('MagentoProduct', 'MagentoWidget','BannerWidget', 'ForeignPost');
@@ -18,7 +18,7 @@
 		static $restrict_for_everyone = true;
 
 		static $migrations = array(
-
+			'0.2' => 'ayvp_settings'
 		);
 
 		static $query_vars = array(
@@ -41,6 +41,15 @@
 			return static::main_site() == get_current_blog_id();
 		}
 
+		static function migrate_ayvp_settings(){
+			global $wpdb;
+			$wpdb->delete($wpdb->options, array('option_name' => 'tern_wp_youtube'));
+			$wpdb->insert($wpdb->options, array(
+				'option_name' => 'tern_wp_youtube',
+				'option_value' => 'a:15:{s:7:"publish";s:1:"0";s:12:"display_meta";s:1:"0";s:5:"words";s:2:"20";s:4:"dims";a:2:{i:0;s:3:"506";i:1;s:3:"304";}s:7:"related";s:1:"0";s:6:"inlist";s:1:"0";s:4:"cron";s:1:"6";s:4:"user";s:0:"";s:8:"channels";a:2:{i:1;a:6:{s:2:"id";i:1;s:4:"name";s:0:"";s:7:"channel";s:0:"";s:4:"type";s:7:"channel";s:10:"categories";a:1:{i:0;s:1:"1";}s:6:"author";s:1:"1";}i:0;a:1:{s:7:"channel";s:18:"progviagemcultural";}}s:3:"rss";i:0;s:5:"limit";i:4;s:5:"pages";i:0;s:11:"last_import";i:1406047847;s:12:"is_importing";b:0;s:7:"version";i:206;}',
+				'autoload' => 'yes'
+			));
+		}
 	}
 
  ?>
